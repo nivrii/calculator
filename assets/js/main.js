2360715b72b7614726fc0028ca0bcb2ed82cb990
nivrii <muchokiirvin@gmail.com>
2022-08-20 16:20:41 +0300

    Fourth commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,5 @@ let screenDisplay = (val) => { return document.getElementById('resultt').value+=
 let result = () => {
 let x = document.getElementById('resultt').value;
 let y = eval(x);
-document.getElementById('resultt').value = y;
-return y;
+return document.getElementById('resultt').value = y;
 }

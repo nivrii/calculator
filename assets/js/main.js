b677e7db447b82a9ed113aa00d4e4910c76a16d4
nivrii <muchokiirvin@gmail.com>
2022-08-20 16:05:08 +0300

    third upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-// *** Write your code here***
+
+let screenDisplay = (val) => { return document.getElementById('resultt').value+= val}
+
+let result = () => {
+let x = document.getElementById('resultt').value;
+let y = eval(x);
+document.getElementById('resultt').value = y;
+return y;
+}
